349eb063e59f37e67bf445b3a1e9d28daaa84eca
Amal Chegdali <chegamal04022003@email.com>
2026-01-02 15:18:48 +0100

    Modification sur branche dev -App.java-

--- demo-app/src/main/java/com/devops/App.java
+++ demo-app/src/main/java/com/devops/App.java
@@ -2,7 +2,7 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour et bon courage dans le projet en DevOps");
+        System.out.println("Bonjour, bon courage dans le projet en DevOps");
     }
 }
 
